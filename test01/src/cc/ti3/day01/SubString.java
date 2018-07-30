403a8381165892465d94c917ead32b3c4501c8e9
jiqing <ji_qing@outlook.com>
2018-07-30 21:59:59 +0800

    新建substring方法的测试，截取最后一个字符串

---
+++ test01/src/cc/ti3/day01/SubString.java
@@ -0,0 +1,12 @@
+package cc.ti3.day01;
+
+public class SubString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="a,b,c,d,";
+		System.out.println(s.substring(0, s.length()-1));
+
+	}
+
+}
